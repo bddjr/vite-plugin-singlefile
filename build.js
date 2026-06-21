07230defd5a69c0310dc43ceed736e0533220c88
bddjr <bddjr08@outlook.com>
2026-06-21 12:34:37 +0800

    build.js

--- build.js
+++ build.js
@@ -1,7 +1,10 @@
 import path from 'path'
+import { execSync } from 'child_process'
 
 import { build as rolldownBuild } from 'rolldown'
 import { dts } from 'rolldown-plugin-dts'
+
+execSync('tsc', { stdio: 'inherit' });
 
 await rolldownBuild({
     input: [
@@ -22,4 +25,9 @@ await rolldownBuild({
     plugins: [
         dts()
     ],
+})
+
+execSync('node --run build', {
+    stdio: 'inherit',
+    cwd: path.resolve('./website/'),
 })
